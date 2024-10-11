ccd7353e6e77068205aa9d6bf036cb04d20713df
prodanserega <74560554+prodanserega@users.noreply.github.com>
2024-10-11 21:37:57 +0300

    swiper

--- js/slider.js
+++ js/slider.js
@@ -1,14 +1,14 @@
 const swiper = new Swiper('.swiper-container', {
   loop: true,
   slidesPerView: 1,
-  spaceBetween: 20,
+  spaceBetween: 30,
   breakpoints: {
     768: {
-      slidesPerView:2,
+      slidesPerView: 2,
     },
 
     1280: {
-      slidesPerView:3,
+      slidesPerView: 3,
     },
   },
 
@@ -19,7 +19,7 @@ const swiper = new Swiper('.swiper-container', {
   },
 
   navigation: {
-    nextEl: '.carousel-button.next',
     prevEl: '.carousel-button.prev',
+    nextEl: '.carousel-button.next',
   },
 });
